9cb5340a60a9e676755cdb8a8613f9d5a157847c
Reid Welliver <rwelliver@enterrasolutions.com>
2017-05-23 17:11:56 -0400

    basic functionality

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var newElement = document.createElement('h1');
+newElement.innerHTML = "Hello!"
+
+document.body.appendChild(newElement);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var path = require( 'path' );
+var webpack = require( 'webpack' );
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  devtool: 'eval-source-map',
+  devServer: {
+   headers: { "Access-Control-Allow-Origin": "*" }
+  },
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:4200',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+          template: 'src/index.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    {
+      test: /\.(mp4|png|ogv|gif|webm|jpg)(\?[a-z0-9#=&.]+)?$/,
+      loader: "file?name=[name].[ext]",
+      include: path.join(__dirname, 'assets')
+    },
+    {
+      test: /\.js$/,
+      loaders: [ 'babel-loader' ],
+      include: path.join(__dirname, 'src')
+    },
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+	},
+    {
+      test: /\.less$/,
+      loader: "style-loader!css-loader!less-loader"
+    }
+    ]
+  },
+  node: {
+    console: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    console: true
+  }
+};
